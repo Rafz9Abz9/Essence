b62ca2cb7a2afb19abf3f9b81c17b7a954a0f5b8
Rafz9Abz9 <rafatramoni15@gmail.com>
2024-02-08 10:18:23 +0000

    implement product model and AWS S3 Setup

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category,Product,Image
 
 # Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = Product.images.through
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from PIL import Image as PilImage
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_image_file_extension
 
 # Create your models here.
+def validate_image_content(value):
+    try:
+        img = PilImage.open(value)
+    except PilImage.UnidentifiedImageError:
+        raise ValidationError("File is not a valid image.")
+    
+    
+class Category(models.Model):
+    name = models.CharField(max_length=25, null=False)
+    friendly_name = models.CharField(max_length=50, null=False, default=" ")
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    # Fields to store information about each image
+    image_file = models.ImageField(
+        upload_to='product_images/',
+        validators=[validate_image_file_extension, validate_image_content]
+    )
+    caption = models.CharField(max_length=255, blank=True)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption 
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    ingredients = models.TextField(null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.BigIntegerField(default=0, null=False)
+    sold = models.BigIntegerField(default=0, null=False)
+    is_featured = models.BooleanField(default=False)
+    images = models.ManyToManyField('Image', related_name='products', blank=True)
+
+
+    def __str__(self):
+        return self.name
