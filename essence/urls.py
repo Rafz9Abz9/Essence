4c265751f47e834643a8c9c1054b8c1ac8a7a038
Rafz9Abz9 <rafatramoni15@gmail.com>
2024-02-28 23:48:49 +0000

    fixing deployment bug

--- essence/urls.py
+++ essence/urls.py
@@ -22,3 +22,7 @@ urlpatterns = [
     path('activate/<uidb64>/<token>', VerificationView.as_view(), name='activate'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
