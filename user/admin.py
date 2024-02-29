23468036606b8053a4eba7f5d19232c46e4ffbc4
Rafz9Abz9 <rafatramoni15@gmail.com>
2024-02-29 11:43:53 +0000

    fixed bug error

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import CustomUser, ShippingAddress
+from .models import CustomUser 
 
 
 admin.site.register(CustomUser)
-admin.site.register(ShippingAddress)
+# admin.site.register(ShippingAddress)
